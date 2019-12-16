96018181b5d7a83e7898140f2357a9b9f068fd56
Scott Renz <scottrenz@scottrenz.com>
2019-12-16 16:26:47 -0500

    did remove_from_end, get_max

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -90,7 +90,10 @@ class DoublyLinkedList:
     current tail's previous node the new tail of the List.
     Returns the value of the removed Node."""
     def remove_from_tail(self):
-        pass
+        value = self.tail.value
+        self.delete(self.tail)
+        return value
+
 
     """Removes the input node from its current spot in the 
     List and inserts it as the new head node of the List."""
@@ -131,4 +134,11 @@ class DoublyLinkedList:
         
     """Returns the highest value currently in the list"""
     def get_max(self):
-        pass
+        current = self.head
+        max_value = current.value
+        while current is not None:
+            if current.value > max_value:
+                max_value = current.value
+            current = current.next
+        return max_value
+            
