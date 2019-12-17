7ead4dae4e2243b8ba74ff21bad1f42676937039
Scott Renz <scottrenz@scottrenz.com>
2019-12-16 19:02:11 -0500

    did push, pop, len for stack, but says should be 2 no 11

--- queue_and_stack/dll_stack.py
+++ queue_and_stack/dll_stack.py
@@ -2,17 +2,22 @@ import sys
 sys.path.append('../doubly_linked_list')
 from doubly_linked_list import DoublyLinkedList
 
+dll = DoublyLinkedList()
+
 class Stack:
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
         # self.storage = ?
-
+        self.storage = dll
     def push(self, value):
-        pass
+        return self.storage.add_to_tail(value)
 
     def pop(self):
-        pass
+        if self.storage.length > 0:
+            return self.storage.remove_from_tail()
+        return
+
 
     def len(self):
-        pass
+        return self.storage.length
