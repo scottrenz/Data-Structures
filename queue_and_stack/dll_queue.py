a80afa28fed9b1c61e3c6cc2123d1ff45144264f
Scott Renz <scottrenz@scottrenz.com>
2019-12-17 14:19:58 -0500

    decrement and increment size

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -2,7 +2,6 @@ import sys
 sys.path.append('../doubly_linked_list')
 from doubly_linked_list import DoublyLinkedList
 
-dll = DoublyLinkedList()
 
 class Queue:
     def __init__(self):
@@ -11,12 +10,14 @@ class Queue:
         # self.storage = ?
         self.storage = DoublyLinkedList()
     def enqueue(self, value):
+        self.size +=1
         return self.storage.add_to_tail(value)
 
     def dequeue(self):
         if self.storage.length > 0:
+            self.size -=1
             return self.storage.remove_from_head()
         return
 
     def len(self):
-        return self.storage.length
+        return self.size
--- queue_and_stack/dll_stack.py
+++ queue_and_stack/dll_stack.py
@@ -2,7 +2,6 @@ import sys
 sys.path.append('../doubly_linked_list')
 from doubly_linked_list import DoublyLinkedList
 
-dll = DoublyLinkedList()
 
 class Stack:
     def __init__(self):
@@ -11,13 +10,15 @@ class Stack:
         # self.storage = ?
         self.storage = DoublyLinkedList()
     def push(self, value):
+        self.size +=1
         return self.storage.add_to_tail(value)
 
     def pop(self):
         if self.storage.length > 0:
+            self.size -=1
             return self.storage.remove_from_tail()
         return
 
 
     def len(self):
-        return self.storage.length
+        return self.size
